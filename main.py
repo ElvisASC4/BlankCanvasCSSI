0923627e3ff6667736f9759c5d789c2e05737f67
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 16:57:49 -0400

    defining post method for new post

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ class MakePost(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render())
 
+    def post(self):
+        # Use the user input to create a new blog post
+        artist_input = self.request.get('artist')
+        title_input = self.request.get('title')
+        poem_input = self.request.get('poem')
+
+
+        new_post = Post(artist= artist_input, title=title_input, poem=poem_input)
+        new_post.put()
+
+
 class ViewPage(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template("template/view.html")
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from google.appengine.ext import ndb
 
 class PostPost(ndb.Model):
   artist = ndb.StringProperty()
+  title = ndb.StringProperty()
   poem = ndb.StringProperty()
+
   # post = ndb.TextProperty()
-  

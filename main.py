f1a014ef8ee33b0c4f4ccfe3f841b7ad2f714ad8
cssi <cssi@cssis-MacBook-Pro-5.local>
2019-07-23 08:11:08 -0700

    Added welcome page class

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
+import jinja2
+import os
+import webapp2
 
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-
-class ViewPage(webapp2.RequestHandler):
-    def get(self):
-        template = the_jinja_enviroment.get_template(" .html")
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render())
-        
-class ViewPage(webapp2.RequestHandler):
-    def get(self):
-
-
-
-
-
-app = webapp2.WSGIApplication([
-    # This line routes the main url ('/')  - also know as
-    # The root route - to the Fortune Handler
-    ('/', WelcomePage),
-    ('/View', View),
-    ('/NewPost', NewPost),
-    ('/friends', FriendsPage) #maps '/predict' to the FortuneHandler
-], debug=True)
+class WelcomePage(webapp2.RequestHandler):
+def get(self):
+        template = jinja_env.get_template('main.html')
+        template_vars = {

5c3eaf2fddfc4b719d8dd400449f62ae262e6b4a
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 11:11:56 -0400

    combined Welcome + View

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ class WelcomePage(webapp2.RequestHandler):
 def get(self):
         template = jinja_env.get_template('main.html')
         template_vars = {
+
+
+
+class ViewPage(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_enviroment.get_template(" .html")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+
+class NewPost(webapp2.RequestHandler):
+    def get(self):
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    # This line routes the main url ('/')  - also know as
+    # The root route - to the Fortune Handler
+    ('/', WelcomePage),
+    ('/View', View),
+    ('/NewPost', NewPost),
+    ('/friends', FriendsPage) #maps '/predict' to the FortuneHandler
+], debug=True)
